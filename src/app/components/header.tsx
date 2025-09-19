af689a243343968e550d46e6e9cc324932791a91
Judith Osei-Tete <jao92@cornell.edu>
2025-09-19 15:57:40 -0400

    Create component for header

---
+++ src/app/components/header.tsx
@@ -0,0 +1,32 @@
+export default function Header() {
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 flex justify-center items-center w-full py-4 shadow-sm hidden md:flex">
+      <nav className="flex items-center space-x-15">
+        <a
+          href="#home"
+          className="text-lg font-medium text-white hover:text-red-300 transition-colors duration-200"
+        >
+          Home
+        </a>
+        <a
+          href="#our-story"
+          className="text-lg font-medium text-white hover:text-red-300 transition-colors duration-200"
+        >
+          Our Story
+        </a>
+        <a
+          href="#program"
+          className="text-lg font-medium text-white hover:text-red-300 transition-colors duration-200"
+        >
+          Program Outline
+        </a>
+        <a
+          href="#support"
+          className="text-lg font-medium text-white hover:text-red-300 transition-colors duration-200"
+        >
+          Support Us
+        </a>
+      </nav>
+    </header>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
+import Header from "./components/header";
+
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="hidden md:flex flex-none w-full border-[0.1rem] border-red-500">
-        Header
-      </header>
+      <Header />
 
       <main className="w-full h-[120vh] flex items-center justify-center parallax relative text-white">
         <h1 className="text-5xl relative z-10">Freeman Jr weds Akosua</h1>
       </main>
 
-      <section className="flex-none w-full p-8">
+      <section className="flex-none w-full p-8 border-t-[0.1rem] border-black">
         <p className="text-5xl">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
           dolor sit amet, consectetur adipiscing elit.. Lorem ipsum dolor sit
